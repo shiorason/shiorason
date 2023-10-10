65faf7e2a41d9cd329c6f8de9d72791574c09188
しお <147463615+shiorason@users.noreply.github.com>
2023-10-10 13:23:44 +0900

    Create soin.py

---
+++ soin.py
@@ -0,0 +1,22 @@
+miyu = input()
+mi = int(miyu)
+
+soin = []
+while mi % 2 ==0:
+  mi = mi // 2
+  soin.append(2)
+
+f = 3
+while f ** 2 <= mi:
+  if mi % f ==0:
+    mi = mi // f
+    soin.append(f)
+  else:
+    f = f + 2
+
+if mi != 1:
+  soin.append(mi)
+
+sopr = str(soin)
+sopt = sopr[1:-1]
+print(sopt.replace(","," *"))
