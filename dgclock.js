2e27636fca04d5f79be0ca41225a1dab6cfd9afb
しお <147463615+shiorason@users.noreply.github.com>
2023-10-16 13:38:33 +0900

    Create dgclock.js

---
+++ dgclock.js
@@ -0,0 +1,26 @@
+const clock = () => {
+  const d = new Date();
+  let ye = d.getFullYear();
+  let mo = d.getMonth() + 1;
+  let da = d.getDate();
+  let daymia = d.getDay();
+  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let day = weekday[daymia];
+  let hour = d.getHours();
+  let min = d.getMinutes();
+  let sec = d.getSeconds();
+
+  mo = mo < 10 ? "0" + mo : mo;
+  da = da < 10 ? "0" + da : da;
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let miaa = `${ye}.${mo}.${da} ${day}`;
+  let miab = `${hour}:${min}:${sec}`;
+  
+  document.querySelector(".clock-date").innerText = miaa;
+  document.querySelector(".clock-time").innerText =miab;
+};
+
+setInterval(clock, 1000);
